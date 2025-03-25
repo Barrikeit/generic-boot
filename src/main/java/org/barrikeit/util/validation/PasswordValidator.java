85ef4f9c8ea09d371e2c8fb65c561f12539916c7
Alberto Barral <abarraltigeras@gmail.com>
2025-03-25 13:03:43 +0100

    password validator

--- src/main/java/org/barrikeit/util/validation/PasswordValidator.java
+++ src/main/java/org/barrikeit/util/validation/PasswordValidator.java
@@ -7,19 +7,15 @@ public class PasswordValidator implements ConstraintValidator<ValidPassword, Str
 
   @Override
   public boolean isValid(String value, ConstraintValidatorContext context) {
-
-    // # start-of-string
     // (?=.*[0-9]) # a digit must occur at least once
     // (?=.*[a-z]) # a lower case letter must occur at least once
     // (?=.*[A-Z]) # an upper case letter must occur at least once
     // (?=.*[@#$%^&+=]) # a special character must occur at least once
     // (?=\S+$) # no whitespace allowed in the entire string
     // .{8,} # anything, at least eight places though
-    // $ # end-of-string
 
-    boolean noExistePassword = value == null || value.isEmpty();
-    // NOTA: Solo en el caso de que exista el password se evalua la expresion regular
-    return noExistePassword
+    return value == null
+        || value.isEmpty()
         || value.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$");
   }
 }
