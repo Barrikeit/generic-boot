0be369bc2c1f0c31b7f766963a5db9dd9c208398
Alberto Barral <abarraltigeras@gmail.com>
2025-09-23 23:42:30 +0200

    filters
    add genericFilter and genericFilterBuilder

--- src/main/java/org/barrikeit/service/dto/GenericDto.java
+++
@@ -1,7 +0,0 @@
-package org.barrikeit.service.dto;
-
-import jakarta.persistence.MappedSuperclass;
-import java.io.Serializable;
-
-@MappedSuperclass
-public class GenericDto implements Serializable {}
