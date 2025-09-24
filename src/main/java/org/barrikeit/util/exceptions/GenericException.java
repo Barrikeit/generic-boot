ad3ad5bd656b5d66e56603638402d49df3886e0d
Alberto Barral <abarraltigeras@gmail.com>
2025-09-24 15:36:42 +0200

    exception control

--- src/main/java/org/barrikeit/util/exceptions/GenericException.java
+++ src/main/java/org/barrikeit/util/exceptions/GenericException.java
@@ -3,10 +3,8 @@ package org.barrikeit.util.exceptions;
 import java.io.Serial;
 import java.net.URI;
 import lombok.extern.log4j.Log4j2;
-import org.barrikeit.util.constants.ExceptionConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.ErrorResponseException;
 
 @Log4j2
